67aa3a154b77da7e319d0c7d1279c8f8ea0f5b8c
Benjamin Massey <benjamin.w.massey@gmail.com>
2025-06-18 00:49:10 -0700

    Added sub-captures in order to get particular parts of the screen.

--- src/capture.rs
+++ src/capture.rs
@@ -1,4 +1,4 @@
-use image::{ImageBuffer, Rgb};
+use image::{GenericImage, ImageBuffer, Rgb};
 use nokhwa::{pixel_format::RgbFormat, utils::*, Camera};
 
 const CAMERA_INDEX_MAX: u32 = 10;
@@ -33,4 +33,40 @@ pub fn get_camera(index: u32) -> Option<Camera> {
 pub fn get_video_frame(camera: &mut Camera) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     let frame = camera.frame().unwrap();
     frame.decode_image::<RgbFormat>().unwrap()
+}
+
+pub fn get_placement_image(frame: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    frame.sub_image(
+        (0.8368f32 * (CAMERA_RESOLUTION.0 as f32)) as u32,
+        (0.7791f32 * (CAMERA_RESOLUTION.1 as f32)) as u32,
+        (0.1289f32 * (CAMERA_RESOLUTION.0 as f32)) as u32,
+        (0.1944f32 * (CAMERA_RESOLUTION.1 as f32)) as u32,
+    ).to_image()
+}
+
+pub fn get_first_item_image(frame: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    frame.sub_image(
+        (0.0906f32 * (CAMERA_RESOLUTION.0 as f32)) as u32,
+        (0.0458f32 * (CAMERA_RESOLUTION.1 as f32)) as u32,
+        (0.0977f32 * (CAMERA_RESOLUTION.0 as f32)) as u32,
+        (0.1708f32 * (CAMERA_RESOLUTION.1 as f32)) as u32,
+    ).to_image()
+}
+
+pub fn get_second_item_image(frame: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    frame.sub_image(
+        (0.0273f32 * (CAMERA_RESOLUTION.0 as f32)) as u32,
+        (0.0472f32 * (CAMERA_RESOLUTION.1 as f32)) as u32,
+        (0.0540f32 * (CAMERA_RESOLUTION.0 as f32)) as u32,
+        (0.0958f32 * (CAMERA_RESOLUTION.1 as f32)) as u32,
+    ).to_image()
+}
+
+pub fn get_coin_image(frame: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    frame.sub_image(
+        (0.0601f32 * (CAMERA_RESOLUTION.0 as f32)) as u32,
+        (0.9028f32 * (CAMERA_RESOLUTION.1 as f32)) as u32,
+        (0.0414f32 * (CAMERA_RESOLUTION.0 as f32)) as u32,
+        (0.0514f32 * (CAMERA_RESOLUTION.1 as f32)) as u32,
+    ).to_image()
 }
--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,23 @@ fn main() {
     };
 
     let mut camera: nokhwa::Camera = capture::get_camera(index).expect("Failed to get capture.");
-    let frame = capture::get_video_frame(&mut camera);
-    let _ = frame.save("test.png");
-    println!("Test frame saved to test.png.");
+
+    let mut frame = capture::get_video_frame(&mut camera);
+    let _ = frame.save(&format!("test_full.png"));
+
+    let place = capture::get_placement_image(&mut frame);
+    let _ = place.save(&format!("test_place.png"));
+
+    let first_item = capture::get_first_item_image(&mut frame);
+    let _ = first_item.save(&format!("test_first.png"));
+
+    let second_item = capture::get_second_item_image(&mut frame);
+    let _ = second_item.save(&format!("test_second.png"));
+
+    let coin = capture::get_coin_image(&mut frame);
+    let _ = coin.save(&format!("test_coin.png"));
+
+    println!("Test frames saved.");
 
     println!("Starting Twitch client...");
     twitch::run();
